916896526cdbc9fbe271412404102ae09c1d6d9f
Night-Owl-Knight <117783808+Night-Owl-Knight@users.noreply.github.com>
2025-07-11 15:24:16 -0500

    Update evilfizzbuzz by adding uneccessary pointers

--- tools/EvilFizzBuzz/C++/evilFizzBuzz.cpp
+++ tools/EvilFizzBuzz/C++/evilFizzBuzz.cpp
@@ -5,8 +5,10 @@
 
 
 int main() {
+    int start = 1, stop = 100, step = 1, fizzFreq =3, buzzFreq=5;
+    std::string fizz = "Fizz", buzz ="Buzz";
 
-    modularFunctionFizzBuzz(1, 100, 1, 3, 5, "Fizz", "Buzz");
+    modularFunctionFizzBuzz(&start, &stop, &step, &fizzFreq, &buzzFreq, &fizz, &buzz);
 
 
     return 0;
--- tools/EvilFizzBuzz/C++/evilFizzBuzz.h
+++ tools/EvilFizzBuzz/C++/evilFizzBuzz.h
@@ -1,22 +1,22 @@
 #include <iostream>
 
-void modularFunctionFizzBuzz(int start, int stop, int step, int fizzFrequency, int buzzFrequency, const std::string fizz, const std::string buzz);
+void modularFunctionFizzBuzz(int* start, int *stop, int* step, int* fizzFrequency, int* buzzFrequency, const std::string* fizz, const std::string* buzz);
 
 
 
 
-void modularFunctionFizzBuzz(int start, int stop, int step, int fizzFrequency, int buzzFrequency, const std::string fizz, const std::string buzz) {
+void modularFunctionFizzBuzz(int* start, int* stop, int* step, int* fizzFrequency, int* buzzFrequency, const std::string* fizz, const std::string* buzz) {
 
-    for (int i =start; i <= stop; i +=step) {
-        if (i % fizzFrequency==0) {
-            std::cout<<fizz;
+    for (int i = *start; i <= *stop; i += *step) {
+        if (i % *fizzFrequency==0) {
+            std::cout<<*fizz;
         }
-        if (i % buzzFrequency==0) {
-            std::cout<<buzz;
+        if (i % *buzzFrequency==0) {
+            std::cout<< *buzz;
         }
 
-        if (i % buzzFrequency !=0 && i %fizzFrequency !=0) {
-            std::cout<<i;
+        if (i % *buzzFrequency !=0 && i % *fizzFrequency !=0) {
+            std::cout<< i;
         }
 
         std::cout<<std::endl;
